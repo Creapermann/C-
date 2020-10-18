bc18c2911dbe4872436bf1d0b94bca907f7daffa
Creapermann-Main <creapermann37mail.com>
2020-10-18 02:02:04 +0200

    18.10.2020 // 02:01 // txt files

--- Code-Files/11.10.2020-1/11.10.2020-1.cpp
+++ Code-Files/11.10.2020-1/11.10.2020-1.cpp
@@ -1,12 +1,8 @@
-/*Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
-without any elements with the same value next to each other and preserving the original order of elements.*/
-
-
 #include <unordered_map>
 #include <iostream>
 
 
-void /*std::unordered_map<std::string, std::string>*/ printRightPairs(const std::string& inputString) {
+void printRightPairs(const std::string& inputString) {
 
 	std::unordered_map<char, char> uMap;
 
